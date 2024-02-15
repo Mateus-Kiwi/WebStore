6cd2a6f0c8696135010aaf1de59669b19946f00c
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2024-02-15 16:33:04 -0300

    feat: create user repository, service and controller

---
+++ backend/WebStore.API/WebStore.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Repositories.Base;
+
+namespace WebStore.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+}
