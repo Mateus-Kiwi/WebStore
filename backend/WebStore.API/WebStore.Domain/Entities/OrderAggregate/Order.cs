fb3f63713bfc3bc397c08332d152f361ce2023e4
Augusto <augusto.medeirosdacosta@gmail.com>
2024-02-19 14:20:43 -0300

    feat: add validate methods

--- backend/WebStore.API/WebStore.Domain/Entities/OrderAggregate/Order.cs
+++ backend/WebStore.API/WebStore.Domain/Entities/OrderAggregate/Order.cs
@@ -1,17 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Validation;
 
 namespace WebStore.Domain.Entities.OrderAggregate;
 
-public class Order : BaseEntity
+public sealed class Order : BaseEntity
 {
     [Required]
-    public decimal Total { get; private set; }
+    public decimal SubTotal { get; private set; }
     
-    public Order(Guid id) : base(id)
+    public IReadOnlyList<OrderItemVO> OrderItems { get; private set; }
+    
+    public Order(Guid id, decimal total, IReadOnlyList<OrderItemVO> orderItems) : base(id)
+    {
+        
+        OrderItems = orderItems;
+    }
+
+    private void ValidateTotal(decimal subTotal)
+    {
+        DomainValidationException.When(decimal.IsNegative(subTotal),"Total order price should be positive.");
+        SubTotal = subTotal;
+    }
+
+    private void GetTotal(decimal subTotal)
     {
         
     }
-    
-    
 }
