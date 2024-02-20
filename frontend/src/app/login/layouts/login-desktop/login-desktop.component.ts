126e6880f3415ef9069f0ef140359adc844b36a2
Mateus-Kiwi <mat.dalla2004@gmail.com>
2024-02-20 13:59:53 -0300

    Commit front end test

--- frontend/src/app/login/layouts/login-desktop/login-desktop.component.ts
+++ frontend/src/app/login/layouts/login-desktop/login-desktop.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../shared/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-desktop',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login-desktop.component.html',
   styleUrl: './login-desktop.component.scss'
 })
 export class LoginDesktopComponent {
+  email : string = '';
+  password : string = '';
 
+  constructor (private auth : AuthService) { }
+  ngOnInit() {}
+
+  login(){
+
+    if(this.email == '') {
+      alert('Please enter your email');
+    return;
+    }
+
+    if(this.password == '') {
+      alert('Please enter your password');
+    return;
+    }
+
+    this.auth.login(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
 }
--- frontend/src/app/register/layouts/register-desktop/register-desktop.component.ts
+++ frontend/src/app/register/layouts/register-desktop/register-desktop.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../shared/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-desktop',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './register-desktop.component.html',
   styleUrl: './register-desktop.component.scss'
 })
 export class RegisterDesktopComponent {
+  email : string = '';
+  password : string = '';
 
+  constructor (private auth : AuthService) { }
+  ngOnInit(): void {}
+
+  register(){
+
+    if(this.email == '') {
+      alert('Please enter your email');
+    return;
+    }
+
+    if(this.password == '') {
+      alert('Please enter your password');
+    return;
+    }
+
+    this.auth.register(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
 }
